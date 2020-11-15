86ec6491147de1645a62bdc12ee9f7682369405a
therealkingnull <69044380+therealkingnull@users.noreply.github.com>
2020-11-15 14:51:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,5 +15,13 @@ app.get('/:user', (req, res) => {
   res.json(wlUser(req.params.user, 1));
 });
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    type: 'error', message: err.message
+  });
+});
+
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
